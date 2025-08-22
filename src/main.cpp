5b8ea86a64799a017cb0825089b7e8d12702cdc1
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-08-22 23:11:50 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "internal/widevine/widevine_protocol.pb.h"
 
 int main() {
+  SignedLicenseRequest signedLicenseRequest = SignedLicenseRequest();
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
