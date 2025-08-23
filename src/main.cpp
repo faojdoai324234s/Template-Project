dfe1f5e87fd9ba129da8616272002ff45b927f98
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-08-23 00:17:30 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include <botan/block_cipher.h>
 #include <iostream>
 #include "internal/widevine/widevine_protocol.pb.h"
 
 int main() {
+  std::unique_ptr<Botan::BlockCipher> sessionKeyBlock = Botan::BlockCipher::create_or_throw("AES-128");
   SignedLicenseRequest signedLicenseRequest = SignedLicenseRequest();
   std::cout << "Hello, World!" << std::endl;
   return 0;
