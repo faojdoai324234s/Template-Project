7fe8a474fd70332e772919a4a4e2e115d6bb950f
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-08-24 16:44:34 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <botan/block_cipher.h>
+#include <libavformat/avformat.h>
 #include <iostream>
 #include "internal/widevine/widevine_protocol.pb.h"
 
 int main() {
   std::unique_ptr<Botan::BlockCipher> sessionKeyBlock = Botan::BlockCipher::create_or_throw("AES-128");
   SignedLicenseRequest signedLicenseRequest = SignedLicenseRequest();
+  AVFormatContext* pFormatContext = avformat_alloc_context();
+  if (!pFormatContext) {
+    return -1;
+  }
+  avformat_close_input(&pFormatContext);
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
